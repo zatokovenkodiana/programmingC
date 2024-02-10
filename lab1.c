ace0ef37d10c02da7135bb430c09db9a39175b13
zatokovenkodiana <159605087+zatokovenkodiana@users.noreply.github.com>
2024-02-10 15:50:30 +1000

    lab1.c

--- lab1.c
+++ lab1.c
@@ -1,11 +1,3 @@
-/******************************************************************************
-
-Welcome to GDB Online.
-GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
-C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
-Code, Compile, Run and Debug online from anywhere in world.
-
-*******************************************************************************/
 #include <stdio.h>
 #include <math.h>
 int main()
@@ -19,9 +11,20 @@ int main()
    d=b*b-4*a*c;
    printf("d = %lf", d);
    
+  
+   if (d>0) {
    x1 = (-b + sqrt(d))/(2*a);
    x1 = (-b - sqrt(d))/(2*a);
    
    printf ("x1=%lf x2=%lf \n", x1, x2);
+   }
+   if (d == 0) {
+       x1 = (-b/(2*a));
+       printf ("x1 = %lf \n", x1);
+   }
+   if (d<0) {
+       printf ("KORNEY NET");
+   }
+   
     return 0;
 }
