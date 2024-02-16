3c98f4766b5d77741ba6e2aef1ab0d95e32c4d52
zatokovenkodiana <159605087+zatokovenkodiana@users.noreply.github.com>
2024-02-16 20:19:51 +1000

    Update laba2.c

--- laba2.c
+++ laba2.c
@@ -1,3 +1,4 @@
+1.
 #include <stdio.h>
 
 int main()
@@ -37,4 +38,49 @@ int main()
 
     return 0;
 }
+2.
+#include <stdio.h>
 
+int main()
+{
+  int n=2;
+    double mas[n][n];
+    
+    for (int i=0; i<n; i++)
+        for (int j=0; j<n; j++)
+        {
+            printf("mas[%d][%d] = ", i+1, j+1);
+            scanf("%lf", &mas[i][j]);
+        }
+        printf("\nМатрица:\n");
+        for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            printf("%f  ", mas[i][j]);
+        }
+        printf("\n");
+    }
+    int vozv_matrix[n][n];
+    int i, j, k = 0, l;
+    while (k < 2)
+    {
+        for (i = 0; i < n; i++)
+            for (j = 0; j < n; j++)
+            {
+                vozv_matrix[i][j] = 0;
+                for (l = 0; l < n; l++)
+                    vozv_matrix[i][j] += (mas[i][l] * mas[l][j]);
+                k++;
+            }
+    }
+    for (i = 0; i<n; i++)
+    {
+        for (j = 0; j<n; j++)
+        {
+            printf("%d  ", vozv_matrix[i][j]);
+        }
+        printf("\n");
+    }
+    return 0;
+}
