0655f7ebb20b0689c140f73af45515bf848922e4
zatokovenkodiana <159605087+zatokovenkodiana@users.noreply.github.com>
2024-03-15 19:33:07 +1000

    Create lab10.c

---
+++ lab10.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+#define MAX_NUMBER(arr, size) ({ \
+    int max = arr[0]; \
+    for (int i = 1; i < size; i++) { \
+        if (arr[i] > max) { \
+            max = arr[i]; \
+        } \
+    } \
+    max; \
+})
+
+int main() {
+    int n;
+    printf("Введите количество чисел: ");
+    scanf("%d", &n);
+    
+    int numbers[n];
+    printf("Введите числа через пробел: ");
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &numbers[i]);
+    }
+
+    int maxNumber = MAX_NUMBER(numbers, n);
+    printf("Максимальное число: %d\n", maxNumber);
+    
+    return 0;
+}
