d0923bcf2c3dc4435cf5c992edadcfc4f708e458
zatokovenkodiana <159605087+zatokovenkodiana@users.noreply.github.com>
2024-03-17 17:53:49 +1000

    Update laba5.c

--- laba5.c
+++ laba5.c
@@ -343,3 +343,139 @@ matrix.c
 void printmatrix(duble **m){
     
 }
+
+
+
+____________________________________________________________________
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "matrix.h"
+
+int main() {
+    
+    int n;
+    double **m1 = (double**)malloc(n * sizeof(double*));
+    double **m2 = (double**)malloc(n * sizeof(double*));
+   
+    printf("Введите размер матрицы:");
+    scanf("%d",&n);
+    
+
+    printf("Введите элементы матрицы 1:\n");
+    for (int i = 0; i < n; i++) {
+        m1[i] = (double*)malloc(n * sizeof(double));
+        for (int j = 0; j < n; j++) {
+            scanf("%lf", &m1[i][j]);
+        }
+    }
+
+    printf("Введите элементы матрицы 2:\n");
+    for (int i = 0; i < n; i++) {
+        m2[i] = (double*)malloc(n * sizeof(double));
+        for (int j = 0; j < n; j++) {
+            scanf("%lf", &m2[i][j]);
+        }
+    }
+
+    char operation;
+    printf("Введите операцию (+, -, *): ");
+    scanf(" %c", &operation);
+    
+    double **result;
+    if (operation == '+') {
+        result = matrix_sum(m1, m2, n);
+    } else if (operation == '-') {
+        result = matrix_minus(m1, m2, n);
+    } else if (operation == '*') {
+        result = matrix_product(m1, m2, n);
+    } else {
+        printf("Ошибка!");
+        return 1;
+    }
+ 
+
+    printf("Результат:\n");
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            printf("%lf ", result[i][j]);
+        }
+        printf("\n");
+    }
+
+    
+
+
+ // Очистка памяти
+    for (int i = 0; i < n; i++) {
+        free(m1[i]);
+        free(m2[i]);
+        free(result[i]);
+    }
+    free(m1);
+    free(m2);
+    free(result);
+
+    return 0;
+}
+
+
+
+matrix.h
+#ifndef MATRIX_H
+#define MATRIX_H
+
+double **matrix_sum(double **m1, double **m2, int n);
+double **matrix_minus(double **m1, double **m2, int n);
+double **matrix_product(double **m1, double **m2, int n);
+
+#endif
+
+
+matrix.c
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "matrix.h"
+
+    double **matrix_sum(double **m1, double **m2, int n) {
+    double **result = (double**)malloc(n * sizeof(double*));
+    for (int i = 0; i < n; i++) {
+        result[i] = (double*)malloc(n * sizeof(double));
+        for (int j = 0; j < n; j++) {
+            result[i][j] = m1[i][j] + m2[i][j];
+        }
+    }
+    return result;
+}
+
+    double **matrix_minus(double **m1, double **m2, int n) {
+    double **result = (double**)malloc(n * sizeof(double*));
+    for (int i = 0; i < n; i++) {
+        result[i] = (double*)malloc(n * sizeof(double));
+        for (int j = 0; j < n; j++) {
+            result[i][j] = m1[i][j] - m2[i][j];
+        }
+    }
+    return result;
+}
+
+    double **matrix_product(double **m1, double **m2, int n) {
+    double **result = (double**)malloc(n * sizeof(double*));
+    for (int i = 0; i < n; i++) {
+        result[i] = (double*)malloc(n * sizeof(double));
+        for (int j = 0; j < n; j++) {
+            result[i][j] = 0;
+            for (int k = 0; k < n; k++) {
+                result[i][j] += m1[i][k] * m2[k][j];
+            }
+        }
+    }
+    return result;
+}
+
+
+
+
+
