893db8912756256c6ad278f420ff338e6315fa62
zatokovenkodiana <159605087+zatokovenkodiana@users.noreply.github.com>
2024-04-13 12:54:31 +1000

    Update laba5.c

--- laba5.c
+++ laba5.c
@@ -137,29 +137,7 @@ matrix.c
     }
 }
  ____________________________________________________________
-    void free(double** m1,  int n) {
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-          
-        }
-       
-    }
-}
-    void free(double** m2,  int n) {
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-          
-        }
-       
-    }
-}
-
-    void free(double** result,  int n) {
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-          
-        }
-       
-    }
+ 
+    
 }
 
