77f2c1c2a35af919239dbf1f22912f73c6784dc4
zatokovenkodiana <159605087+zatokovenkodiana@users.noreply.github.com>
2024-04-26 21:39:38 +1000

    Update lab6.c

--- lab6.c
+++ lab6.c
@@ -73,3 +73,37 @@ int main(int argc, char *argv[]) {
 
     return 0;
 }
+_______________________________________________________________________________________________
+
+
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    FILE *fp1, *fp2;
+    /*
+    fp1 = fopen("input.txt", "w");
+    char s[10] = "abcdefghij";
+    char s2[10] = "0123456789";
+    
+    for(int i = 0; i<20; i++){
+        fprintf(fp1, "%c%c %c%c %c%c 19%c%c\n", s[rand()%10],s[rand()%10],s[rand()%10],s[rand()%10],s[rand()%10],s[rand()%10],s2[rand()%10],s2[rand()%10]);
+    }
+    */
+    
+    fp1 = fopen("input.txt", "r");
+    fp2 = fopen("output.txt", "w");
+    char s1[100];
+    
+    while(fgets(s1, 50, fp1)){
+        char s2[100];
+        strncpy(s2, s1+strlen(s1)-5, strlen(s1));
+        if(atoi(s2) > 1980)
+            fprintf(fp2, "%s", s1);
+    }
+    fclose(fp1);
+    fclose(fp2);
+
+    return 0;
+}
