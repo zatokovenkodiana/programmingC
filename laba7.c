d1dd46a8846725e2dac949b004791f7006e2de0e
zatokovenkodiana <159605087+zatokovenkodiana@users.noreply.github.com>
2024-04-27 13:20:10 +1000

    Update laba7.c

--- laba7.c
+++ laba7.c
@@ -43,3 +43,52 @@ int main(){
     }
     return 0;
 }
+_____________________________________________________________________________
+
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+ 
+ struct humen{
+    char name[100], sname[100];
+    int byear;
+};
+
+int main(){
+
+    FILE *input_file = fopen("input.txt", "r"); 
+    FILE *output_file = fopen("output.txt", "w"); 
+
+    int N = 5, i=0;
+    struct humen a[N], b[N], t;
+    char name[100], sname[100], s[100];
+    int byear;
+    
+    while (fgets(s,80,input_file)!=NULL){
+        sscanf(s,"%s %s %d", a[i].name,a[i].sname,&a[i].byear);
+        printf("%s %s %d \n",a[i].name,a[i].sname,a[i].byear);
+        b[i]=a[i];        
+        i++;
+    }
+
+        for(int i = 0; i<N-1; i++){
+            for(int j = 0; j<N-i-1; j++){
+            if(b[j].byear < b[j+1].byear){
+                t = b[j+1];
+                b[j+1] = b[j];
+                b[j] = t;
+            }
+        }}
+        
+    
+    for(int i = 0; i<N; i++){
+         fprintf(output_file, "%d %s %s %d\n", i, b[i].name, b[i].sname, b[i].byear); 
+    }
+    
+
+    fclose(input_file); 
+    fclose(output_file); 
+
+    
+    return 0;
+}
