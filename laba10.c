03475debc4f7a7d948c1860a99bb26bfb07b10d8
zatokovenkodiana <159605087+zatokovenkodiana@users.noreply.github.com>
2024-05-15 22:44:50 +1000

    Create laba10.c

---
+++ laba10.c
@@ -0,0 +1,25 @@
+#include <time.h>
+#include <stdio.h>
+#include <stdarg.h>
+
+int add_num(int n, ...)
+{
+    int res = 0;
+    va_list list;
+    va_start(list, n);
+    while (n)
+    {
+        res += va_arg(list, int);
+        --n;
+    }
+    va_end(list);
+    return res;
+    
+}
+
+int main() {
+    int res = 0;
+    res = add_num(5, 1,2,3,4,5);
+    printf("res=%d", res);
+}
+    
