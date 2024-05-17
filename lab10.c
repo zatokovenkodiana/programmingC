bbde90d7a143b8e32dd5108f6661b41c91ff06ae
zatokovenkodiana <159605087+zatokovenkodiana@users.noreply.github.com>
2024-05-17 22:52:40 +1000

    Update lab10.c

--- lab10.c
+++ lab10.c
@@ -1,17 +1,16 @@
 Написать программу вычисления максимального числа из введенных пользова-телем в виде одной строки.  
  Использовать для этого макрос (без функции), на вход ко-торому подается массив чисел и их количество. 
 
- 
 #include <stdio.h>
 
-#define MAX_NUMBER(arr, size) ({ 
-    int max = arr[0]; 
-    for (int i = 1; i < size; i++) { 
-        if (arr[i] > max) { 
-            max = arr[i]; 
-        } 
-    } 
-    max; 
+#define MAX_NUMBER(arr, size) ({ \
+    int max = arr[0]; \
+    for (int i = 1; i < size; i++) { \
+        if (arr[i] > max) { \
+            max = arr[i]; \
+        } \
+    } \
+    max; \
 })
 
 int main() {
