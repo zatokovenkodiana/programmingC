e0dd0fbb7b4d7e08464fc8c479e0db436b8e01c7
zatokovenkodiana <159605087+zatokovenkodiana@users.noreply.github.com>
2024-05-25 13:13:38 +1000

    Update lab12.c

--- lab12.c
+++ lab12.c
@@ -47,3 +47,51 @@ printf("День недели: %d\n", tm.tm_wday);
 
 return 0;
 }
+_______________________________________________________
+
+#include <stdio.h>
+#include <time.h>
+ 
+int main()
+{   time_t now;
+    struct tm * timeinfo;
+    int year, m, i, j;
+    char* month[] = { "January","February", "March","April", "May","June", 
+            "July", "August","September","October", "November","December"};
+    
+    printf ("YEAR: "); scanf ("%d",&year);
+    time (&now); 
+    timeinfo = localtime (&now);
+    timeinfo->tm_year = year-1900; 
+    timeinfo->tm_mon = 0;
+ 
+    m =-1;
+while(m<12)
+{   for(i=1; i<33; i++) 
+    {   timeinfo->tm_mday = i;
+        mktime (timeinfo);  
+ 
+        if(timeinfo->tm_mon > m) 
+        {   printf("\n                        %s\n",month[timeinfo->tm_mon]);
+            printf("_______________________________\n");
+            printf("  Su  Mo  Tu  We  Th  Fr  Sa\n");
+            printf("_______________________________\n");
+            for(j=0;j<(timeinfo->tm_wday);j++)
+                printf("    ");
+            m++;
+            break;
+        }
+        if (m==11 && timeinfo->tm_mon ==0) 
+        {   m++;
+            break;
+        }
+        
+        printf("%4d",timeinfo->tm_mday); 
+            
+        if(timeinfo->tm_wday==6)
+            printf("\n");
+    }
+}
+    getchar();getchar();
+    return 0;
+}
