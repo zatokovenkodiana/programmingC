8e8a19d1f03a7c63d1ca0494049ebf55cc550f34
zatokovenkodiana <159605087+zatokovenkodiana@users.noreply.github.com>
2024-05-30 18:24:05 +1000

    Update lab9.c

--- lab9.c
+++ lab9.c
@@ -68,3 +68,41 @@ int main()
     
     return 0;
 }
+___________________________________________________________________________
+
+#include <stdio.h> 
+#include <stdlib.h> 
+#include <string.h>
+
+int main() { 
+char input[100]; 
+char *token; 
+double result = 0; 
+double num; 
+char op = '+';
+
+ 
+printf("Enter mathematical expression: ");
+fgets(input, sizeof(input), stdin);
+
+token = strtok(input, " ");
+
+while (token != NULL) {
+    if (sscanf(token, "%lf", &num) == 1) {
+        if (op == '+') {
+            result += num;
+        } else if (op == '-') {
+            result -= num;
+        }
+    } else {
+        if (strlen(token) == 1 && (token[0] == '+' || token[0] == '-')) {
+            op = token[0];
+        }
+    }
+    token = strtok(NULL, " ");
+}
+
+printf("Result: %.1f\n", result);
+
+return 0;
+}
