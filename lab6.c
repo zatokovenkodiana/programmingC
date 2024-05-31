1b4522d464845316998b22df9f9908b96b077e13
zatokovenkodiana <159605087+zatokovenkodiana@users.noreply.github.com>
2024-05-31 19:04:43 +1000

    Update lab6.c

--- lab6.c
+++ lab6.c
@@ -13,17 +13,17 @@ int main() {
     char first_name[20], middle_name[20], last_name[20]; 
     int year; 
 
-    in_file = fopen("input.txt", "r");  //открывает файл для чтения
-    out_file = fopen("output.txt", "w");  //открывает файл и создает пустой файл для записи
+    in_file = fopen("input.txt", "r");  
+    out_file = fopen("output.txt", "w"); 
 
     while (fscanf(in_file, "%s %s %s %d", last_name, first_name, middle_name, &year) == 4) {
         if (year > 1980) {
-            fprintf(out_file, "%s %s %s %d\n", last_name, first_name, middle_name, year);  //если файл заполнен, то читает его и записывает в выходной файл данные, которые удовлетворяют условию
+            fprintf(out_file, "%s %s %s %d\n", last_name, first_name, middle_name, year);  
         }
     }
 
     fclose(in_file);
-    fclose(out_file);  //закрывает файлы
+    fclose(out_file);  
 
     return 0;
 }
