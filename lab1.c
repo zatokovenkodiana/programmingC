90a609a14937f98a524c87c5c8c2e15b56d5d896
zatokovenkodiana <159605087+zatokovenkodiana@users.noreply.github.com>
2024-06-01 13:40:46 +1000

    Update lab1.c

--- lab1.c
+++ lab1.c
@@ -35,3 +35,83 @@ int main()
     } while (i == 1);
     return 0;
 }
+
+
+
+
+-------------------------------------------------------------------------
+
+   
+   
+   #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+
+int random_number(int min, int max) {
+    return rand() % (max - min + 1) + min;
+
+
+int main(int argc, char *argv[]) {
+    time_t t1,t0;
+    long unsigned int i;
+    float diftime;
+
+    t0 = time (NULL);
+    
+      for(i=0; i<1500000000L; i++);
+  //---------------------------------------
+    int rows, cols;
+    
+    printf("Введите количество строк: ");
+    scanf("%d", &rows);
+    printf("Введите количество столбцов: ");
+    scanf("%d", &cols);
+    
+    
+    
+    int matrix1[rows][cols], matrix2[rows][cols];
+    
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            matrix1[i][j] = random_number(1, 100); 
+            matrix2[i][j] = random_number(1, 100);
+        }
+    }
+    
+    FILE *file = fopen("input.txt", "w");
+    if (file == NULL) {
+        printf("ошибка\n");
+        return 1;
+    }
+    
+     for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            fprintf(file, "%d ", matrix1[i][j]);
+        }
+        fprintf(file, "\n");
+    }
+    fprintf(file, "\n");
+   
+    fclose(file);
+    
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            matrix1[i][j] = 0;
+            matrix2[i][j] = 0;
+        }
+    }
+
+    return 0;
+}
+    
+ 
+    
+    //--------------------------------------------
+   
+   t1 = time (NULL);
+    diftime = difftime( t1, t0);
+    printf("Need time difftime = %f \n", difftime); 
+    
+    return 0;
+}
